bfff387f26eb87a19b2110fdce8f894e806c0a17
Jaime Lara Carrillo <G00425663@atu.ie>
2023-04-16 17:37:07 +0100

    testing csv format visualitation and basic.py

--- analysis.py
+++ analysis.py
@@ -1,2 +1,14 @@
-# Analysis 
+# Analysis.py
 # Author: Jaime Lara Carrillo
+
+# Import all usefull libraries fot the analysis
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import pandas as pd
+
+
+df = pd.read_csv("iris_csv.csv")    # Read a CSV file named "iris_csv" into a Pandas DataFrame named "df"
+
+df = df.describe()                  # This outputs basic stadistic information
+print(df)
